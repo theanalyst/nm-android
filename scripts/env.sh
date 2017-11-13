7a07a9a5b0708f9508f9efb6cebd9e77b8f09b86
Aurelien Aptel <aaptel@suse.com>
2017-11-13 15:51:11 +0100

    add NMDEV string in front of shell prompt in the env
    Signed-off-by: Aurelien Aptel <aaptel@suse.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 export NM_ROOT=$(readlink -f $(dirname "$BASH_SOURCE")/..)
 export NM_BUILD=$NM_ROOT/build/arm64-libcpp-23/
+
+if ! echo "$PS1" | grep -q 'NMDEV'; then
+    export PS1="(NMDEV) $PS1"
+fi
+
