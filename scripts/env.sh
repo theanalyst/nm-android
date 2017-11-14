d5d4ad5d8cdc9d96e09b3c3e1216e7c63477583f
Aurelien Aptel <aaptel@suse.com>
2017-11-14 14:21:34 +0100

    add gettext, glib, libffi, iconv configure script + env flag tweaks
    Signed-off-by: Aurelien Aptel <aaptel@suse.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -22,7 +22,7 @@ export LD=$target_host-ld
 export STRIP=$target_host-strip
 
 # Tell configure what flags Android requires.
-export CFLAGS="-fPIE -fPIC -I$NL_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64 -isystem $NM_BUILD/include/c++/4.9.x"
+export CFLAGS="-fPIE -fPIC -I$NL_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64"
 export CXXFLAGS="-fPIE -fPIC -I$NL_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64 -isystem $NM_BUILD/include/c++/4.9.x"
 export LDFLAGS="-pie -L$NM_OUT/lib"
 
