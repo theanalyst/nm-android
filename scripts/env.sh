6eeb9e0b7b9fb670f8e0b0819ff380df7758a2ca
Aurelien Aptel <aaptel@suse.com>
2017-11-14 20:07:47 +0100

    add gettext-runtime, tweak iconv and env
    Signed-off-by: Aurelien Aptel <aaptel@suse.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -31,8 +31,8 @@ export LD=$target_host-ld
 export STRIP=$target_host-strip
 
 # Tell configure what flags Android requires.
-export CFLAGS="-fPIE -fPIC -I$NL_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64"
-export CXXFLAGS="-fPIE -fPIC -I$NL_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64 -isystem $NM_BUILD/include/c++/4.9.x"
+export CFLAGS="-fPIE -fPIC -I$NM_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64"
+export CXXFLAGS="-fPIE -fPIC -I$NM_OUT/include -D__ANDROID__ -D_LARGEFILE_SOURCE=1 -D_LARGE_FILES -D_FILE_OFFSET_BITS=64 -isystem $NM_BUILD/include/c++/4.9.x"
 export LDFLAGS="-pie -L$NM_OUT/lib"
 
 export PKG_CONFIG_PATH=$NM_OUT/lib/pkgconfig
@@ -42,3 +42,5 @@ export PKG_CONFIG_LIBDIR=$NM_OUT/lib
 function reload() {
     source $NM_SCRIPT/env.sh
 }
+
+alias e="emacs -nw"
