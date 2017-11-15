c91b419dfc6386dbefc0552df18b4414adc6b5d8
Abhishek Lekshmanan <abhishek@suse.com>
2017-11-15 14:26:33 +0100

    scripts: add iconv-detect header file from android emu
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

---
+++ scripts/iconv-detect.h
@@ -0,0 +1,6 @@
+generic_arm64:/data/local # cat iconv-detect.h
+/* This is an auto-generated header, DO NOT EDIT! */
+
+#define ICONV_ISO_INT_FORMAT "iso-%u-%u"
+#define ICONV_ISO_STR_FORMAT "iso-%u-%s"
+#define ICONV_10646 "UCS-4BE"
